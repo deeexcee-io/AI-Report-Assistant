e9670391cf13774505d3eee058c95aca0a73d494
deeexcee-io <130473605+deeexcee-io@users.noreply.github.com>
2023-04-16 17:41:06 +0100

    Update Report-Assist.py

--- Report-Assist.py
+++ Report-Assist.py
@@ -2,7 +2,7 @@ import openai
 import xml.etree.ElementTree as ET
 import sys
 import time
-
+import signal
 
 art="""
               ,---------------------------,
@@ -30,13 +30,13 @@ print(art)
 time.sleep(1)
 
 # Set OpenAI API key
-openai.api_key = "---INSERT API KEY HERE---"
+openai.api_key = "----INSERT API KEY HERE----"
 
 # Initialize message history
 message_history = [
     {"role": "user", "content": "You are a penetration test report writing technical assistant. "
                                  "Give me Penetration Test Report writeups for each vulnerability I give you. "
-                                 "Provide a Description, Background, Risk, Impact and remediaton section. "
+                                 "Provide a Description, Background, Risk, Impact and Remediaton section. "
                                  "All respsonses must be highly technical in nature. show the commands "
                                  "used to check for this issue."}]
 
@@ -52,16 +52,16 @@ def chat_with_gpt3(message_history):
         return reply_content
     except openai.error.RateLimitError as e:
         print(f"Error: {e}")
-        print("API is currently overloaded, your on a free plan so it can happen. Either try again or Upgrade (up to you )")
-        sys.exit()
+        print("API is currently overloaded, you're on a free plan so it can happen. Either try again or Upgrade (up to you )")
+        main()
     except KeyboardInterrupt:
-        print("\n\n**** You want to quit huh?....No problem ****")
+        print("\n\n**** You want to quit huh?....No problem ****\n\n")
         time.sleep(1)
-        sys.exit()
+        main()
     except Exception as e:
         print(f"Error: {e}")
         print("An error occured, im out...")
-        sys.exit()
+        main()
 
 def get_plugin_name(nessus_file):
     """Parse the Nessus XML file and return a list of plugin names"""
@@ -81,12 +81,17 @@ def search_for_vulns():
     """Search for vulnerabilities using GPT-3 API"""
     while True:
         user_input = input("---Interactive--->: ")
-        message_history.append({"role": "user", "content": f"{user_input}"})
-        reply_answer = chat_with_gpt3(message_history)
-        print(reply_answer, "\n\n")
-        print("* - - - Next Vulnerability - - - *\n\n")
-        #Remove the last input to clean the history
-        del message_history[-1]
+        if user_input == "exit":
+            print("Quit requested....exiting\n")
+            time.sleep(0.5)
+            main()
+        else:
+            message_history.append({"role": "user", "content": f"{user_input}"})
+            reply_answer = chat_with_gpt3(message_history)
+            print(reply_answer, "\n\n")
+            print("\n* - - - Next Vulnerability - - - *\n\n[type \"exit\" to return to main menu]\n")
+   		    #Remove the last input to clean the history
+            del message_history[-1]
 
 def import_nessus_file():
     """Import Nessus XML file and generate AI generated report writeups"""
@@ -102,14 +107,20 @@ def import_nessus_file():
                         print("\n********ISSUE********", x, plugin_list, file=f, sep='\n')
                         del message_history[-1]
 
+def signal_handler(signal, frame):
+    print('\n\nYou pressed Ctrl+C!')
+    sys.exit()
+
+signal.signal(signal.SIGINT, signal_handler)
+
 def main():
         print("* - - - -  Technical Report Writing Assistant - - - - *\n")
         print("Do you want to search for Vulnerabilites or Import a Nessus File?\n\n")
         choice = input("""(1) - Search for Vulns \n(2) - Import Nessus File \n\n(1) or (2) : """)
         if choice == "1":
-                print("\nSearch for Vulns Selected\nEnter a search term, be specific for best results")
+                print("\nSearch for Vulns Selected\nEnter a search term, be specific for best results - Type \"exit\" to return to menu\n")
                 search_for_vulns()
-        if choice == "2":
+        elif choice == "2":
                 import_nessus_file()
         else:
                 print("Invalid Choice, exiting....")
